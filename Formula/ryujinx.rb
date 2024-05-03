e1b9add496fd145d0f9b4d8f8962da105c1611dc
Candygoblen123 <andrew.glaze76@gmail.com>
2024-05-02 23:56:17 -0400

    require dotnet-sdk to be installed instead of downloading it ourselves

--- Formula/ryujinx.rb
+++ Formula/ryujinx.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ryujinx < Formula
   desc "Experimental Nintendo Switch Emulator written in C#"
   homepage "https://www.ryujinx.org"
@@ -12,26 +9,14 @@ class Ryujinx < Formula
   # depends_on "dotnet" => :build
 
   def install
-    # brew does not provide 2XX sdk versions in dotnet formula, so we download it manually for now
-    dotnet_url = ""
-    on_arm do
-      dotnet_url = "https://download.visualstudio.microsoft.com/download/pr/8746698c-596d-406e-b672-49a53d77eea7/74c28673e54213d058eec2c9151714cc/dotnet-sdk-8.0.204-osx-arm64.tar.gz"
-    end
-    on_intel do
-      dotnet_url = "https://download.visualstudio.microsoft.com/download/pr/9548c95b-8495-4b69-b6f0-1fdebdbbf9ff/30827786409718c5a9604711661da3b5/dotnet-sdk-8.0.204-osx-x64.tar.gz"
-    end
-    if Formula["dotnet"].any_version_installed?
-      opoo "Downloading dotnet 8.0.204, which brew does not provide. It will only be used for this installation, and will be deleted afterwards."
+    # this is a hack, and should be removed as soon as the dotnet formula is up to date enough
+    if Cask::CaskLoader.load("dotnet-sdk").installed? == false
+      odie "This formulae requires 'homebrew/cask/dotnet-sdk' to be installed.
+       To install it, run:
+         brew install --cask dotnet-sdk"
     end
 
-    ohai "Downloading #{dotnet_url}"
-    `curl #{dotnet_url} -o dotnet_sdk.tar.gz`
-
-    mkdir "dotnet-root"
-    system "tar zxf dotnet_sdk.tar.gz -C #{buildpath}/dotnet-root"
-    ENV.prepend_path "PATH", "#{buildpath}/dotnet-root"
-    ENV.append "DOTNET_ROOT", "#{buildpath}/dotnet-root"
-    ENV.append "DOTNET_CLI_TELEMETRY_OPTOUT", "1"
+    ENV.prepend_path "PATH", "/usr/local/share/dotnet"
 
     git_hash = `git rev-parse --short HEAD`.strip
 
@@ -43,7 +28,6 @@ class Ryujinx < Formula
       s.gsub!("%%RYUJINX_TARGET_RELEASE_CHANNEL_REPO%%", "homebrew-ryujinx")
       s.gsub!("%%RYUJINX_CONFIG_FILE_NAME%%", "Config.json")
     end
-
 
     system "./distribution/macos/create_macos_build_ava.sh . publish_tmp_ava publish_ava ./distribution/macos/entitlements.xml \"#{version}\" \"#{git_hash}\" Release -p:ExtraDefineConstants=\"DISABLE_UPDATER\""
 
@@ -62,15 +46,6 @@ class Ryujinx < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test Ryujinx`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
